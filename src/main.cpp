d4e708515301607b35fbd1b71943f59a4459df8f
jere-dev <jeremthom1@gmail.com>
2025-05-07 20:33:32 -0400

    add glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
 
-int main() {
-  std::cout << "Hello world!\n";
-  return 0;
-}
+int main() { return 0; }
